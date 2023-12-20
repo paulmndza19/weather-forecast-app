fa8516dabed9591b46434ca3825366a53d81a32b
Paul Mendoza <paulmndza19@gmail.com>
2023-12-20 10:02:51 +0800

    feat: Add simple error handling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,11 @@ class HomeController < ApplicationController
   def search
     weather_data = FetchWeatherService.call(city: params[:city])
 
+    if weather_data.nil?
+      render html: "<span class='text-gray-800 dark:text-white font-bold'>City not found </span>".html_safe
+      return
+    end
+
     render partial: "/home/partials/weather_content", locals: {
       weather_data: weather_data,
     }
--- app/services/get_geo_code_service.rb
+++ app/services/get_geo_code_service.rb
@@ -4,20 +4,24 @@ require "net/https"
 class GetGeoCodeService
   class << self
     def retrieve_city_geo_code(city:)
-      uri = URI.parse(ENV.fetch("OPEN_WEATHER_API_GEO_ENDPOINT", "http://api.openweathermap.org/geo/1.0/direct"))
-      params = { appid: ENV.fetch("OPEN_WEATHER_API_KEY", nil), q: city, limit: 1 }
-      uri.query = URI.encode_www_form(params)
+      begin
+        uri = URI.parse(ENV.fetch("OPEN_WEATHER_API_GEO_ENDPOINT", "http://api.openweathermap.org/geo/1.0/direct"))
+        params = { appid: ENV.fetch("OPEN_WEATHER_API_KEY", nil), q: city, limit: 1 }
+        uri.query = URI.encode_www_form(params)
 
-      request = Net::HTTP::Get.new(
-        uri,
-        {
-          "Content-Type" => "application/json",
-        }
-      )
+        request = Net::HTTP::Get.new(
+          uri,
+          {
+            "Content-Type" => "application/json",
+          }
+        )
 
-      result = call_endpoint(uri, request)
+        result = call_endpoint(uri, request)
 
-      { lon: result&.first.lon, lat: result&.first.lat }
+        return { lon: result&.first.lon, lat: result&.first.lat }
+      rescue
+        return nil
+      end
     end
 
     private
